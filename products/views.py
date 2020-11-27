ccbe9a7211fe937d7dd43e266623b28e7de36731
IpoLa <akramchat@hotmail.com>
2020-11-27 15:50:26 +0100

    master

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, JsonResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from .models import Product
+from .forms import ProductModelForm
 # Create your views here.
 
 def home__view(request, *args, **kwargs):
@@ -37,14 +38,34 @@ def search__view(request, *args, **kwargs):
     context = {"home": "abc", "query": query}
     return render(request, "home.html", context)
 
+# def product__create__view(request, *args, **kwargs):
+#     # print(request.POST)
+#     # print(request.GET)
+#     if request.method == "POST":
+#         post_data = request.POST or None
+#         if post_data != None:
+#             my_form = ProductForm(request.POST)
+#             if my_form.is_valid():
+#                 print(my_form.cleaned_data.get("title"))
+#                 title_form_input = my_form.cleaned_data.get("title")
+#                 Product.objects.create(title=title_form_input)
+#                 # print("post_data ", post_data)
+#     return render(request, "forms.html", {})
+
 def product__create__view(request, *args, **kwargs):
-    # print(request.POST)
-    # print(request.GET)
-    if request.method == "POST":
-        post_data = request.POST or None
-        if post_data != None:
-            print("post_data ", post_data)
-    return render(request, "forms.html", {})
+    form = ProductModelForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.user = request.user
+        obj.save()
+        # print(form.cleaned_data)
+        # data = form.cleaned_data
+        # Product.objects.create(**data)
+        form = ProductModelForm()
+        # return HttpResponseRedirect("/success")
+        # return redirect("/success")
+        
+    return render(request, "forms.html", {"form": form})
 
 def product__list__view(request, *args, **kwargs):
     qs = Product.objects.all()
